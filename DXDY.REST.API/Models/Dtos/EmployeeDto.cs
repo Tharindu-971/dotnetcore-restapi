8c8610f42f57195a129378fd81193187115e10f5
Tharindu-971 <htharindu971@gmail.com>
2022-06-02 01:43:59 +0530

    Add Validation to Dtos

--- DXDY.REST.API/Models/Dtos/EmployeeDto.cs
+++ DXDY.REST.API/Models/Dtos/EmployeeDto.cs
@@ -1,9 +1,12 @@
-﻿namespace DXDY.REST.API.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DXDY.REST.API.Models.Dtos
 {
     public class EmployeeDto
     {
 
         public int Id { get; set; }
+        [Required]
         public string Code { get; set; }
         public string Name { get; set; }
         public int? Mobile { get; set; }
